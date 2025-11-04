367199aaa71b9909e69a3b29b7e516f675c24747
amanmanit <anupiitd2016@gmail.com>
2025-11-04 13:03:44 +0530

    Add files via upload

---
+++ models/create_fasterrcnn_model.py
@@ -0,0 +1,328 @@
+from models import *
+
+def return_fasterrcnn_resnet50_fpn(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_resnet50_fpn.create_model(
+        num_classes, pretrained=pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_mobilenetv3_large_fpn(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_mobilenetv3_large_fpn.create_model(
+        num_classes, pretrained=pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_mobilenetv3_large_320_fpn(
+    num_classes, pretrained=True, coco_model=False
+):    
+    model = fasterrcnn_mobilenetv3_large_320_fpn.create_model(
+        num_classes, pretrained=pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_resnet18(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_resnet18.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_custom_resnet(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_custom_resnet.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_darknet(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_darknet.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_squeezenet1_0(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_squeezenet1_0.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_squeezenet1_1(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_squeezenet1_1.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_mini_darknet(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_mini_darknet.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_squeezenet1_1_small_head(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_squeezenet1_1_small_head.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_mini_squeezenet1_1_small_head(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_mini_squeezenet1_1_small_head.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_mini_squeezenet1_1_tiny_head(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_mini_squeezenet1_1_tiny_head.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_mbv3_small_nano_head(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_mbv3_small_nano_head.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_mini_darknet_nano_head(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_mini_darknet_nano_head.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_efficientnet_b0(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_efficientnet_b0.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_nano(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_nano.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_resnet152(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_resnet152.create_model(
+        num_classes, pretrained, coco_model
+    )
+    return model
+
+def return_fasterrcnn_resnet50_fpn_v2(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_resnet50_fpn_v2.create_model(
+        num_classes, pretrained=pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_convnext_small(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_convnext_small.create_model(
+        num_classes, pretrained=pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_convnext_tiny(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_convnext_tiny.create_model(
+        num_classes, pretrained=pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_resnet101(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_resnet101.create_model(
+        num_classes, pretrained=pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_vitdet(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_vitdet.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_vitdet_tiny(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_vitdet_tiny.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_mobilevit_xxs(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_mobilevit_xxs.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_regnet_y_400mf(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_regnet_y_400mf.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_vgg16(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_vgg16.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_convnext_tiny(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_convnext_tiny.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_vits16(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_vits16.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_convnext_tiny_multifeat(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_convnext_tiny_multifeat.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_vits16plus(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_vits16plus.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_vitb16(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_vitb16.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_vitl16(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_vitl16.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_vith16plus(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_vith16plus.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_convnext_small(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_convnext_small.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_convnext_base(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_convnext_base.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+def return_fasterrcnn_dinov3_convnext_large(
+    num_classes, pretrained=True, coco_model=False
+):
+    model = fasterrcnn_dinov3_convnext_large.create_model(
+        num_classes, pretrained, coco_model=coco_model
+    )
+    return model
+
+create_model = {
+    'fasterrcnn_resnet50_fpn': return_fasterrcnn_resnet50_fpn,
+    'fasterrcnn_mobilenetv3_large_fpn': return_fasterrcnn_mobilenetv3_large_fpn,
+    'fasterrcnn_mobilenetv3_large_320_fpn': return_fasterrcnn_mobilenetv3_large_320_fpn,
+    'fasterrcnn_resnet18': return_fasterrcnn_resnet18,
+    'fasterrcnn_custom_resnet': return_fasterrcnn_custom_resnet,
+    'fasterrcnn_darknet': return_fasterrcnn_darknet,
+    'fasterrcnn_squeezenet1_0': return_fasterrcnn_squeezenet1_0,
+    'fasterrcnn_squeezenet1_1': return_fasterrcnn_squeezenet1_1,
+    'fasterrcnn_mini_darknet': return_fasterrcnn_mini_darknet,
+    'fasterrcnn_squeezenet1_1_small_head': return_fasterrcnn_squeezenet1_1_small_head,
+    'fasterrcnn_mini_squeezenet1_1_small_head': return_fasterrcnn_mini_squeezenet1_1_small_head,
+    'fasterrcnn_mini_squeezenet1_1_tiny_head': return_fasterrcnn_mini_squeezenet1_1_tiny_head,
+    'fasterrcnn_mbv3_small_nano_head': return_fasterrcnn_mbv3_small_nano_head,
+    'fasterrcnn_mini_darknet_nano_head': return_fasterrcnn_mini_darknet_nano_head,
+    'fasterrcnn_efficientnet_b0': return_fasterrcnn_efficientnet_b0,
+    'fasterrcnn_nano': return_fasterrcnn_nano,
+    'fasterrcnn_resnet152': return_fasterrcnn_resnet152,
+    'fasterrcnn_resnet50_fpn_v2': return_fasterrcnn_resnet50_fpn_v2,
+    'fasterrcnn_convnext_small': return_fasterrcnn_convnext_small, 
+    'fasterrcnn_convnext_tiny': return_fasterrcnn_convnext_tiny,
+    'fasterrcnn_resnet101': return_fasterrcnn_resnet101,
+    'fasterrcnn_vitdet': return_fasterrcnn_vitdet,
+    'fasterrcnn_vitdet_tiny': return_fasterrcnn_vitdet_tiny,
+    'fasterrcnn_mobilevit_xxs': return_fasterrcnn_mobilevit_xxs,
+    'fasterrcnn_regnet_y_400mf': return_fasterrcnn_regnet_y_400mf,
+    'fasterrcnn_vgg16': return_fasterrcnn_vgg16,
+    'fasterrcnn_dinov3_convnext_tiny': return_fasterrcnn_dinov3_convnext_tiny,
+    'fasterrcnn_dinov3_vits16': return_fasterrcnn_dinov3_vits16,
+    'fasterrcnn_dinov3_convnext_tiny_multifeat': return_fasterrcnn_dinov3_convnext_tiny_multifeat,
+    'fasterrcnn_dinov3_vits16plus': return_fasterrcnn_dinov3_vits16plus,
+    'fasterrcnn_dinov3_vitb16': return_fasterrcnn_dinov3_vitb16,
+    'fasterrcnn_dinov3_vitl16': return_fasterrcnn_dinov3_vitl16,
+    'fasterrcnn_dinov3_vith16plus': return_fasterrcnn_dinov3_vith16plus,
+    'fasterrcnn_dinov3_convnext_small': return_fasterrcnn_dinov3_convnext_small,
+    'fasterrcnn_dinov3_convnext_base': return_fasterrcnn_dinov3_convnext_base,
+    'fasterrcnn_dinov3_convnext_large': return_fasterrcnn_dinov3_convnext_large
+}
---
+++ models/fasterrcnn_dinov3_vitb16.py
@@ -0,0 +1,97 @@
+import sys
+import os
+import torch
+import torchvision
+import torch.nn as nn
+
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+from models.model_summary import summary
+
+# Get current file's absolute path
+current_file = os.path.abspath(__file__)
+
+# Get the directory of the current file (models folder)
+current_dir = os.path.dirname(current_file)
+
+# Get the parent directory (previous directory)
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.append(os.path.join(parent_dir, 'dinov3'))
+
+# Relative to parent fasterrcnn directory.
+REPO_DIR = 'dinov3'
+# Relative to parent fasterrcnn directory or the absolute path.
+WEIGHTS_URL = 'weights/dinov3_vitb16_pretrain_lvd1689m-73cec8be.pth'
+
+class Dinov3Backbone(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = torch.hub.load(
+            REPO_DIR, 
+            'dinov3_vitb16', 
+            source='local', 
+            weights=WEIGHTS_URL
+        )
+
+        self.out_indices = [3, 6, 9, 11]
+    
+    def forward(self, x):
+        out = self.backbone.get_intermediate_layers(
+            x, n=self.out_indices, reshape=True, return_class_token=False, norm=True
+        )
+        
+        return {
+            '0': out[0],
+            '1': out[1],
+            '2': out[2],
+            '3': out[3],
+        }
+    
+
+def create_model(num_classes=81, pretrained=True, coco_model=False):
+    backbone = Dinov3Backbone()
+
+    backbone.out_channels = 768
+
+    for name, params in backbone.named_parameters():
+        params.requires_grad_(False)
+
+    # Generate anchors using the RPN. Here, we are using 5x3 anchors.
+    # Meaning, anchors with 5 different sizes and 3 different aspect 
+    # ratios.
+    anchor_generator = AnchorGenerator(
+        sizes=((32, 64, 128, 256, 512),) * 4,
+        aspect_ratios=((0.5, 1.0, 2.0),) * 4
+    )
+
+    # Feature maps to perform RoI cropping.
+    # If backbone returns a Tensor, `featmap_names` is expected to
+    # be [0]. We can choose which feature maps to use.
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+        featmap_names=['0', '1', '2', '3'],
+        output_size=7,
+        sampling_ratio=2
+    )
+
+    # Final Faster RCNN model.
+    model = FasterRCNN(
+        backbone=backbone,
+        num_classes=num_classes,
+        rpn_anchor_generator=anchor_generator,
+        box_roi_pool=roi_pooler
+    )
+    return model
+
+if __name__ == '__main__':
+    from models.model_summary import summary
+
+    model = create_model(81, pretrained=True)
+
+    print(model)
+    
+    random_tensor = torch.randn(1, 3, 640, 640)
+
+    _ = model.eval()
+
+    summary(model)
